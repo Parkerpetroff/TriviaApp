670783385c9fa7f10ca4b44de0f7cc4f4c1e5aab
Parker Petroff <32392444+Parkerpetroff@users.noreply.github.com>
2024-05-14 10:44:38 -0400

    Add Project Files

---
+++ TrivaApp/TrivaApp.iOS/SQLiteiOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using TrivaApp.iOS;
+using TrivaApp.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SQLiteiOS))]
+namespace TrivaApp.iOS
+{
+public class SQLiteiOS : ISQLite
+    {
+        public SQLiteConnection GetSQLiteConnection()
+        {
+            var sqliteFilename = "testing.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libraryPath = Path.Combine(documentsPath, "..", "Library");
+            var path = Path.Combine(libraryPath, sqliteFilename);
+
+            var conn = new SQLiteConnection(path);
+            return conn;
+        }
+    }
+}
+
